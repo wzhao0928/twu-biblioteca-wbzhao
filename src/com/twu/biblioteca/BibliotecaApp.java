02a2453dae2fa32715fb309e5049b429bdef3bbe
wzhao0928 <wbzhao@thoughtworks.com>
2015-04-12 07:30:35 +0800

    [wbzhao] Default Welcome Message and the corresponding test

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,13 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.ui.WelcomeMessage;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(new WelcomeMessage().getWelcomeMessage());
+//        TODO：List Books - As a customer, after the welcome message appears I would like to see a list of all library books, so that I can browse for books I might want to check-out. Assume that there is a pre-existing list of books. You don't need to support adding or removing books from the library.
+//        TODO: A pre-existing list of book
+//        TODO: print the pre-existing list of book
     }
 }
