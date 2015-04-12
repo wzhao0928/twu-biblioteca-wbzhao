111dfdc4da7b69832f5a21d1b021fbb07be3dcd5
wzhao0928 <wbzhao@thoughtworks.com>
2015-04-12 23:30:57 +0800

    [wbzhao] Command line test

---
+++ test/com/twu/biblioteca/ui/CommandLineTests.java
@@ -0,0 +1,20 @@
+package com.twu.biblioteca.ui;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by wbzhao on 15-4-12.
+ */
+public class CommandLineTests {
+    @Test
+    public void test_command_line_should_return_option_according_to_input() throws Exception {
+        CommandLine commandLine = new CommandLine();
+        System.setIn(new ByteArrayInputStream("List Books".getBytes()));
+        assertEquals(Option.LIST_BOOKS, commandLine.readCommand());
+        System.setIn(System.in);
+    }
+}
